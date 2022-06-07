613deec15fdf9906747556ddd8c5136942c24130
Leonardo Matos <leomp120894@gmail.com>
2022-06-06 21:05:42 -0300

    Setup astro with vue + partytown + tailwind + stiemap

---
+++ storefront/astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+
+import vue from "@astrojs/vue";
+import tailwind from "@astrojs/tailwind";
+import turbolinks from "@astrojs/turbolinks";
+import partytown from "@astrojs/partytown";
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [vue(), tailwind(), turbolinks(), partytown(), sitemap()]
+});
